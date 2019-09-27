49029b310c02b0053401581b09a25a163289f1f0
hiago <foto.hiago@gmail.com>
2019-09-27 17:48:43 -0300

    create routers

--- heroWiki/urls.py
+++ heroWiki/urls.py
@@ -15,7 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+from rest_framework import routers
+
+from habilidade.views import HabilidadeViewSet
+from hcategoria.views import CategoriaViewSet
+
+router = routers.DefaultRouter()
+router.register(r'habilidade', HabilidadeViewSet)
+router.register(r'categoria', CategoriaViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
 ]
